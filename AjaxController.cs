1bbc5012fbc20a36ba8674b66a4d6541c84c0556
Arnildo Nieves <arnildonieves@gmail.com>
2015-10-12 13:06:25 -0300

    Primera subida de archivos

---
+++ AjaxController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploMVCMio.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult ImportarPagina()
+        {
+            return View();
+        }
+
+        public ActionResult ImportarSeccion()
+        {
+            return View();
+        }
+
+        public ActionResult AjaxSeccion()
+        {
+            return View();
+        }
+
+        public ActionResult PaginaContenido()
+        {
+            return View();
+        }
+
+        public JsonResult FechaHoraAjax(string id)
+        {
+            try
+            {
+                return Json(new 
+                {
+                    Fecha = DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss"),
+                    Estado = "Ok",
+                    Variable = id
+                });
+            }
+            catch (Exception)
+            {
+                return Json(new { Estado = "Error" });
+                throw;
+            }
+        }
+    }
+}
---
+++ JqueryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EjemploMVCMio.Controllers
+{
+    public class JqueryController : Controller
+    {
+        public ActionResult Selector()
+        {
+            return View();
+        }
+    }
+}
